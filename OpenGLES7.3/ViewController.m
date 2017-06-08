eee4c6e2c83521976d80d25d04123b2a7c2f28ae
Chenshiwen <cswzmg@live.com>
2017-06-08 16:55:45 +0800

    Initial Commit

---
+++ OpenGLES7.3/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OpenGLES7.3
+//
+//  Created by ShiWen on 2017/6/8.
+//  Copyright © 2017年 ShiWen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ OpenGLES7.3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OpenGLES7.3
+//
+//  Created by ShiWen on 2017/6/8.
+//  Copyright © 2017年 ShiWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
